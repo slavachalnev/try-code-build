2ddd61b7ea3f373770431cd49a3b1792d43c2094
Slava Chalnev <slava.chalnev@gmail.com>
2023-06-12 20:24:26 +1000

    Add gpu test

---
+++ tests/gpu_test.py
@@ -0,0 +1,10 @@
+import torch
+
+def test_gpu():
+    assert torch.cuda.is_available()
+
+    x = torch.randn(2, 3).cuda()
+    y = torch.randn(2, 3).cuda()
+    z = x + y
+    assert z.shape == (2, 3)
+
